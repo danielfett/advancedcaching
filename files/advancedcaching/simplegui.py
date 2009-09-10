0ad8dab84bd7e5c3fb5102b327d871140948b837
Daniel Fett <fett@danielfett.de>
2009-09-10 20:30:20 +0200

    Added Copyright notice

--- files/advancedcaching/simplegui.py
+++ files/advancedcaching/simplegui.py
@@ -15,7 +15,7 @@
 #   You should have received a copy of the GNU General Public License
 #   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-#	Author: Daniel Fett simplecaching@fragcom.de
+#	Author: Daniel Fett advancedcaching@fragcom.de
 #
 
  
