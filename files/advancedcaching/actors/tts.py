ee95ce6cd60b6cad003ff9ff80839e8b65aa6869
Daniel Fett <fett@danielfett.de>
2010-05-29 17:24:15 +0200

    Towards the TTS integration

--- files/advancedcaching/actors/tts.py
+++ files/advancedcaching/actors/tts.py
@@ -4,7 +4,10 @@ import gobject
 class TTS(gobject.GObject):
 
     def __init__(self, core):
-        core.connect('good-fix', self.__tell_distance)
+        self.gps_target_bearing = None
+        self.gps_target_distance = None
+        core.connect('good-fix', self.__on_good_fix)
+        gobject.timeout_add_seconds(20, self.__tell)
         gobject.GObject.__init__(self)
 
         try:
@@ -15,8 +18,18 @@ class TTS(gobject.GObject):
 
         self.proc.stdin.write("Espeak ready.\n")
 
+    def __on_good_fix(self, caller, gps_data, distance, bearing):
+        self.gps_target_distance = distance
+        self.gps_target_bearing = bearing
 
-    def __tell_distance(self, caller, gps_data, distance, bearing):
-        output = "%d meters." % distance
+    def __tell(self):
+        if self.gps_target_distance == None:
+            return True
+        output = "%d meters, %d o'clock.\n" % (self.gps_target_distance, self.__degree_to_hour(self.gps_target_bearing))
         self.proc.stdin.write(output)
-        print "Speaking: %s" % output
+        return True
+
+    @staticmethod
+    def __degree_to_hour(degree):
+        ret = round((degree%360)/30.0)
+        return ret if ret != 0 else 12
