441610535451a77552ef57c1f604f454868adc1d
Daniel Fett <fett@danielfett.de>
2011-01-19 21:53:07 +0100

    Version changed for pypi

--- files/setup.py
+++ files/setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 from setuptools import setup
 files = ['data/*', 'actors/*.py', 'qt/*.py']
 setup(name='agtl',
-	version='0.8.0.1-freerunner0',
+	version='0.8.0.3-freerunner0',
 	description='Towards paperless geocaching',
 	author='Daniel Fett',
 	author_email='agtl@fragcom.de',
