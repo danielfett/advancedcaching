902032d0998d463583281d0334cf31c6d595070f
Daniel Fett <daniel@gamma.(none)>
2012-07-29 16:36:26 +0200

    Add CLI test script

---
+++ scripts/test-cli.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+EXE=advancedcaching/core.py
+$EXE update
+$EXE import --in 'q:Hemmersdorf' 'q:Niedaltdorf'
+$EXE import --around 'q:Olewig' 0.6
+# The routing option is not maintained at the moment
+# $EXE import --at-route 'q:Olewig' 'q:Trier' 0.1
+$EXE filter -f -s small do --print
+$EXE filter -t 2.0 do --fetch-details
+$EXE filter -o r:'.*ebhamste.*' do --command "echo %s"
+$EXE filter -d 2.0 do --commands "echo {name} {difficulty} {size} {owner}"
