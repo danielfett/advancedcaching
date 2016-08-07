5c08f2e11107a6950532606a30e71bc8e70019bc
Aapo Rantalainen <aapo.rantalainen@gmail.com>
2016-08-07 22:10:18 +0300

    Meego: use launch.sh instead of launch
    launch.sh is detected automatically as a shell script, which is good

--- build/make-meego-debuild.sh
+++ build/make-meego-debuild.sh
@@ -22,7 +22,7 @@ mkdir -p $PKGTMP
 
 cp -a $PKGROOT/* $PKGTMP/
 mkdir -p $PKGTMP/src/opt/advancedcaching/
-mv $PKGTMP/launch $PKGTMP/src/opt/advancedcaching/
+mv $PKGTMP/launch.sh $PKGTMP/src/opt/advancedcaching/
 cp changelog $PKGTMP/debian/
 # Copy python sources 
 rsync -av --delete --exclude='*.pyc' \
@@ -54,7 +54,6 @@ cp $RES/advancedcaching-80.png $PKGTMP/src/usr/share/icons/hicolor/80x80/apps/ad
 cd $PKGTMP/
 debuild --no-lintian -aarmel 
 cd -
-#echo "Now run PATH=\$PATH:`pwd`/aegis-builder/ perl aegis-builder/aegis-deb-util --verbose --add-manifest --manifest=meego-debuild/_aegis --add-digsigsums='/opt/advancedcaching/launch' <debfile>"
 echo "Running aegis-build:"
-PATH=$PATH:`pwd`/aegis-builder/ perl aegis-builder/aegis-deb-util --verbose --add-manifest --manifest=meego-debuild/_aegis --add-digsigsums='/opt/advancedcaching/launch' `ls $PKG/*.deb`
+PATH=$PATH:`pwd`/aegis-builder/ perl aegis-builder/aegis-deb-util --verbose --add-manifest --manifest=meego-debuild/_aegis --add-digsigsums='/opt/advancedcaching/launch.sh' `ls $PKG/*.deb`
 
